0d345f8814aa299839f35cb02e36ea3e79fee7b2
cdunahee36 <dunahee10@gmail.com>
2020-10-11 15:44:41 -0500

    Added 2 arrays for upper and lower case letters.

--- script.js
+++ script.js
@@ -12,3 +12,35 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+var lowerCaseArray = ["a","b","c","d","e","f","g"
+"h","i","j","k","l","m","n","o","p","q","r",
+"s","t","u","v","w","x","y","z"];
+
+var upperCaseArray = ["A","B","C","D","E","F","G"
+"H","I","J","K","L","M","N","O","P","Q","R",
+"S","T","U","V","W","X","Y","Z"];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
