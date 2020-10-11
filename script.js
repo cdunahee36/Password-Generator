8a3eab044ded0586f43b25dc872afcb9f231651b
cdunahee36 <dunahee10@gmail.com>
2020-10-11 15:52:55 -0500

    Added 2 more arrays for numbers and special characters.

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() 
+
+var lowerCaseArray = ["a","b","c","d","e","f","g"
+"h","i","j","k","l","m","n","o","p","q","r",
+"s","t","u","v","w","x","y","z"];
+
+var upperCaseArray = ["A","B","C","D","E","F","G"
+"H","I","J","K","L","M","N","O","P","Q","R",
+"S","T","U","V","W","X","Y","Z"];
+
+var numbArray = ["0","1","2","3","4","5","6","7","8","9"];
+
+var specialArray = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,21 +29,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-var lowerCaseArray = ["a","b","c","d","e","f","g"
-"h","i","j","k","l","m","n","o","p","q","r",
-"s","t","u","v","w","x","y","z"];
-
-var upperCaseArray = ["A","B","C","D","E","F","G"
-"H","I","J","K","L","M","N","O","P","Q","R",
-"S","T","U","V","W","X","Y","Z"];
-
-
-
-
-
-
 
 
 
